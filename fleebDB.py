c8c352dc2fbc6f3bd6399264f1eff802f3e5fa6c
Wyatt Ingraham <s540693@nwmissouri.edu>
2024-11-13 15:06:36 -0600

    added some input validation for the signup

--- fleebDB.py
+++ fleebDB.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 #for this import, copy the next line in below this into the terminal to have it set up with your mongoDB
 import random
-
+import re
 
 #pip install pymongo
 
@@ -22,34 +22,50 @@ gather = db["loginInfo"]
 
  
 def signup():
+    
+    #this pattern will be used for login as well
+    pattern  = r'^[A-Za-z\s]{1,20}$'
+
+    def username_validate(us):
+        return bool(re.match(pattern, us))
+
 
     #for this input segment, we will integrate gathering user data through the website for the username and password portion
     #after we do this, we will plug this into the data base.
 
     username = input("enter a username: ")
-    #after the user inputs their username, we will check to see if that name is in use
-    search = gather.find_one({"username": username}, {"username": 1, "_id": 0})
-    name = search["username"] if search else None
-    if(name !=  username):
+    if(username_validate(username) == True):
 
-        #if the username is available, they will be prompted to enter a password
-        password = input("enter a password: ")
-        confirm_password = input("re-enter your password: ")
 
-        if(password ==  confirm_password):
-            print("Your username is: ", username, "\nYour password is: ", password)
+        #this will also be used for login
+        username = username.lower()
 
-            #this will be a unique number assigned to each user.
-            #userID = random.randint()
-            userID = 0
-            new_user = {"userID": userID,"username": username, "password": password,}
-            gather.insert_one(new_user)
-        else:
-            print("passwords do not match")
-    
+
+        #after the user inputs their username, we will check to see if that name is in use
+        search = gather.find_one({"username": username}, {"username": 1, "_id": 0})
+        name = search["username"] if search else None
+        if(name !=  username):
+
+            #if the username is available, they will be prompted to enter a password
+            password = input("enter a password: ")
+            confirm_password = input("re-enter your password: ")
+
+            if(password ==  confirm_password):
+                print("Your username is: ", username, "\nYour password is: ", password)
+
+                #this will be a unique number assigned to each user.
+                #userID = random.randint()
+                userID = 0
+                new_user = {"userID": userID,"username": username, "password": password,}
+                gather.insert_one(new_user)
+            else:
+                print("passwords do not match")
         
+            
+        else:
+            print("This username is already registered.")
     else:
-        print("This username is already registered.")
+        print("Invalid username. Username must be between 1 and 20 letters with spaces allowed.")
 
 
 def log_in():
