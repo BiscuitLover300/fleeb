7f56c1fea060b14a0be6ccbeec56cf8a2df5a49d
Wyatt Ingraham <s540693@nwmissouri.edu>
2024-11-22 09:47:25 -0600

    Changed how the funcitons work to work with flask

--- fleebDB.py
+++ fleebDB.py
@@ -1,3 +1,5 @@
+from flask import Flask, request, jsonify, render_template, redirect
+
 from pymongo import MongoClient
 #for this import, copy the next line in below this into the terminal to have it set up with your mongoDB
 import random
@@ -6,6 +8,8 @@ import re
 #pip install pymongo
 
 #After doing so, the code should run fine
+
+app = Flask(__name__)
 
 
 client = MongoClient("mongodb://localhost:27017/")
@@ -18,99 +22,66 @@ db = client["UserInfo"]
 gather = db["loginInfo"]
 
 
+#This is the valdation used in signup and login
+def username_validate(username):
+    pattern = r'^[A-Za-z\s]{1,20}$'
+    return bool(re.match(pattern, username))
 
 #this function is how users will set up an account to check out. We could make an option of checkout to make this not mandatory to buy stuff.
  
+
+# Route for rendering the signup page
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    
-    #this pattern will be used for login as well
-    pattern  = r'^[A-Za-z\s]{1,20}$'
+    if request.method == 'POST':
+        username = request.form.get('username').lower()
+        password = request.form.get('password')
+        confirm_password = request.form.get('confirm_password')
 
-    def username_validate(us):
-        return bool(re.match(pattern, us))
+        if not username_validate(username):
+            return jsonify({"error": "Invalid username. Must be 1-20 letters with spaces allowed."}), 400
 
+        if gather.find_one({"username": username}):
+            return jsonify({"error": "Username already exists."}), 400
 
-    #for this input segment, we will integrate gathering user data through the website for the username and password portion
-    #after we do this, we will plug this into the data base.
+        if password != confirm_password:
+            return jsonify({"error": "Passwords do not match."}), 400
 
-    username = input("enter a username: ")
-    if(username_validate(username) == True):
+        user_id = gather.estimated_document_count() + 1
+        new_user = {"userID": user_id, "username": username, "password": password}
+        gather.insert_one(new_user)
 
-
-        #this will also be used for login
-        username = username.lower()
-
-
-        #after the user inputs their username, we will check to see if that name is in use
-        search = gather.find_one({"username": username}, {"username": 1, "_id": 0})
-        name = search["username"] if search else None
-        if(name !=  username):
-
-            #if the username is available, they will be prompted to enter a password
-            password = input("enter a password: ")
-            confirm_password = input("re-enter your password: ")
-
-            if(password ==  confirm_password):
-                print("Your username is: ", username, "\nYour password is: ", password)
-
-                #this will be a unique number assigned to each user.
-                #userID = random.randint()
-                userID = 0
-                new_user = {"userID": userID,"username": username, "password": password,}
-                gather.insert_one(new_user)
-            else:
-                print("passwords do not match")
-        
-            
-        else:
-            print("This username is already registered.")
-    else:
-        print("Invalid username. Username must be between 1 and 20 letters with spaces allowed.")
+        return jsonify({"message": "Account created successfully!"}), 200
+    return render_template('signup.html')
 
 
 
 #this will be the login function on the website that will return the users data to them after they have set up an account
 
-def log_in():
-     
-    pattern  = r'^[A-Za-z\s]{1,20}$'
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form.get('username').lower()
+    password = request.form.get('password')
 
-    #password_pattern = 
+    user = gather.find_one({"username": username, "password": password})
+    if user:
+        return jsonify({"message": f"Welcome, {username}!"}), 200
+    return jsonify({"error": "Invalid username or password."}), 401
 
-    def username_validate(us):
-        return bool(re.match(pattern, us))
+# Route to serve the login page
+@app.route('/login_page', methods=['GET'])
+def login_page():
+    return render_template('login.html')
 
-    username = input("enter your username: ")
+# Home route (redirects to login)
+@app.route('/')
+def home():
+    return redirect('/login_page')
 
-    if(username_validate(username) == True):
-
-        username = username.lower()
-
-
-        search = gather.find_one({"username": username}, {"username": 1, "_id": 0})
-        name = search["username"] if search else None
-        if(name ==  username):
-
-
-            password = input("enter your password: ")
-
-            pass_search = gather.find_one({"password": password}, {"password": 1, "_id": 0})
-            word =  pass_search["password"] if search else None
-
-            if(word == password):
-                print("Welcome " + username + "!")
-            else:
-                print("incorrect password, please try again.")
-        else:
-            print("No entry of this user, if you want to make an account, click sign up!")
-    else:
-        print("No vaild user entered, please try again.")
-
+if __name__ == "__main__":
+    app.run(debug=True)
         
 
-
-#signup()
-#log_in()
 
 
             
