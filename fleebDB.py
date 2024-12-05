9f1751e67d94f9f86db4e2270fada2bd5ec9ed4a
Wyatt Ingraham <s540693@nwmissouri.edu>
2024-12-05 10:32:14 -0600

    Different version of fleebDB
    This version is more to try and integrate the two sides together. I have the other python only version saved as well if we need to bring it back.

--- fleebDB.py
+++ fleebDB.py
@@ -1,129 +1,41 @@
-from flask import Flask, request, jsonify, render_template, redirect
-
+from flask import Flask, request, jsonify
 from pymongo import MongoClient
-#for this import, copy the next line in below this into the terminal to have it set up with your mongoDB
-import random
 import re
 
-#pip install pymongo
+app = Flask(__name__)
 
-#After doing so, the code should run fine
-
-#app = Flask(__name__)
-
-
+# MongoDB setup
 client = MongoClient("mongodb://localhost:27017/")
-
 db = client["UserInfo"]
-#collection = db["InfoList"]
-
-
-#This is where we will store login info
 gather = db["loginInfo"]
 
-
-#This is the valdation used in signup and login
+# Function to validate the username (same as your earlier logic)
 def username_validate(username):
     pattern = r'^[A-Za-z\s]{1,20}$'
     return bool(re.match(pattern, username))
 
-#this function is how users will set up an account to check out. We could make an option of checkout to make this not mandatory to buy stuff.
- 
-
-# Route for rendering the signup page
-#@app.route('/signup', methods=['GET', 'POST'])
-
+# Route for handling the signup
+@app.route('/signup', methods=['POST'])
 def signup():
+    data = request.get_json()  # Get the JSON data from the request
+    username = data.get('username', '').strip().lower()  # Clean and lowercased username
+    password = data.get('password', '').strip()
 
+    # Validate username
+    if not username_validate(username):
+        return jsonify({'success': False, 'message': 'Invalid username. It must be between 1 and 20 characters and contain only letters and spaces.'})
 
+    # Check if the username already exists in the database
+    existing_user = gather.find_one({"username": username})
+    if existing_user:
+        return jsonify({'success': False, 'message': 'This username is already taken.'})
 
-    #for this input segment, we will integrate gathering user data through the website for the username and password portion
-    #after we do this, we will plug this into the data base.
+    # If username is valid and doesn't exist, add to database
+    new_user = {"username": username, "password": password}
+    gather.insert_one(new_user)
 
-    username = input("enter a username: ")
-    if(username_validate(username) == True):
+    return jsonify({'success': True, 'message': 'Account created successfully!'})
 
-
-        #this will also be used for login
-        username = username.lower()
-
-
-        #after the user inputs their username, we will check to see if that name is in use
-        search = gather.find_one({"username": username}, {"username": 1, "_id": 0})
-        name = search["username"] if search else None
-        if(name !=  username):
-
-            #if the username is available, they will be prompted to enter a password
-            password = input("enter a password: ")
-            confirm_password = input("re-enter your password: ")
-
-            if(password ==  confirm_password):
-                print("Your username is: ", username, "\nYour password is: ", password)
-
-                #this will be a unique number assigned to each user.
-                #userID = random.randint()
-                userID = 0
-                new_user = {"userID": userID,"username": username, "password": password,}
-                gather.insert_one(new_user)
-            else:
-                print("passwords do not match")
-        
-            
-        else:
-            print("This username is already registered.")
-    else:
-        print("Invalid username. Username must be between 1 and 20 letters with spaces allowed.")
-
-
-
-#this will be the login function on the website that will return the users data to them after they have set up an account
-
-def log_in():
-     
-
-    username = input("enter your username: ")
-
-    if(username_validate(username) == True):
-
-        username = username.lower()
-
-
-        search = gather.find_one({"username": username}, {"username": 1, "_id": 0})
-        name = search["username"] if search else None
-        if(name ==  username):
-
-
-            password = input("enter your password: ")
-
-            pass_search = gather.find_one({"password": password}, {"password": 1, "_id": 0})
-            word =  pass_search["password"] if search else None
-
-            if(word == password):
-                print("Welcome " + username + "!")
-            else:
-                print("incorrect password, please try again.")
-        else:
-            print("No entry of this user, if you want to make an account, click sign up!")
-    else:
-        print("No vaild user entered, please try again.")
-
-        
-
-
-#signup()
-#log_in()
-
-
-            
-
-#This part is for getting the database and collection put onto ur mongo for the first time
-
-#sample_data = {"name": "Alice", "age": 30, "city": "New York"}
-#collection.insert_one(sample_data)
-#print("Data inserted successfully!")
-
-
-# This will pull the information and display it in ur terminal
-
-#document = collection.find_one({"name": "Alice"})
-#print("Retrieved document:", document)
+# Run the Flask app
+if __name__ == '__main__':
+    app.run(debug=True)
